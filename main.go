bf2b339e1a442e16d081da014adbb5f3eb940715
Kanokorn <kanokorn.may@gmail.com>
2016-09-29 16:00:26 +0700

    change logic calculate to say greeting

--- main.go
+++ main.go
@@ -5,25 +5,20 @@ import "fmt"
 import "time"
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-	midnigth := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
-	midday := time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, time.Local)
-	sunset := time.Date(now.Year(), now.Month(), now.Day(), 18, 0, 0, 0, time.Local)
+	hour := time.Now().Hour()
 
-	if (now.After(midnigth) || now.Equal(midnigth)) && now.Before(midday) {
+	if hour < 12 {
 		fmt.Fprint(w, "Hello, Good morning.")
-	}
-
-	if now.After(midday) && now.Before(sunset) {
+	} else if hour < 18 {
 		fmt.Fprint(w, "Hello, Good afternoon.")
-	}
-
-	if now.After(sunset) && now.Before(midnigth) {
+	} else {
 		fmt.Fprint(w, "Hello, Good evening.")
 	}
 }
 
 func main() {
+	fmt.Println("Started call API on http://localhost:8888/greetings")
+
 	http.HandleFunc("/greetings", sayHello)
 	http.ListenAndServe(":8888", nil)
 }
